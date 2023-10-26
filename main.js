393795c55ee0d245c662722fe24a558943989c26
kulklex <kulklex@gmail.com>
2023-10-26 22:46:27 +0100

    Implemented sort functionality

--- main.js
+++ main.js
@@ -1,11 +1,13 @@
 
 const app = Vue.createApp({
-    
-    
+
     data() {
         return {
             cartPage: false,
             checkoutPage: false,
+            ascending: true,
+            descending: false,
+            activeBtn: "",
             cart: [],
             lessons: [
                 {
@@ -211,10 +213,15 @@ const app = Vue.createApp({
             } else {
                 this.cart.push(lesson)
             }
+            console.log(this.ascending, this.descending)
         },
         removeFromCart(lesson) {
             this.cart.splice(this.cart.indexOf(lesson), 1)
             ++lesson.spaces
+            if(!this.cart.length || this.cart.length === 0) {
+                this.checkoutPage = false;
+                this.cartPage = false;
+            }
         },
         clearCart() {
             this.cart = [];
@@ -228,6 +235,104 @@ const app = Vue.createApp({
         changeToCheckoutPage() {
             this.checkoutPage = !this.checkoutPage;
             this.cartPage = false;
-        }
+        },
+        changeToAscending() {
+            this.descending = false;
+            this.ascending = true;
+            this.activeBtn = 'asc'
+        },
+        changeToDescending() {
+            this.ascending = false;
+            this.descending = true;
+            this.activeBtn = 'dsc'
+        },
+        sortPrices() {
+            if(this.ascending){
+                let sortedSubs = this.lessons
+                sortedSubs = sortedSubs.sort((a, b) => {
+                    return a.price - b.price;
+                })
+            }
+            if (this.descending) {
+                let sortedSubs = this.lessons
+                sortedSubs = sortedSubs.sort((a, b) => {
+                    return b.price - a.price
+                })
+            }
+        },
+        sortLocations() {
+            if (this.ascending) {
+                let sortedSubs = this.lessons
+            sortedSubs = sortedSubs.sort((a, b) => {
+                let subsA = a.location.toLowerCase()
+                let subsB = b.location.toLowerCase()
+                if (subsA < subsB) {
+                    return -1
+                }
+                if (subsA > subsB) {
+                    return 1
+                }
+                return 0
+            })
+            }   
+            if (this.descending) {
+                let sortedSubs = this.lessons
+                sortedSubs = sortedSubs.sort((a, b) => {
+                    let subsA = a.location.toLowerCase()
+                    let subsB = b.location.toLowerCase()
+                    if (subsA < subsB) {
+                        return 1
+                    }
+                    if (subsA > subsB) {
+                        return -1
+                    }
+                    return 0
+                })
+            }
+        },
+        sortSpaces() {
+            if(this.ascending) {
+                let sortedSubs = this.lessons
+                sortedSubs = sortedSubs.sort((a, b) => {
+                    return a.spaces - b.spaces;
+                })
+            }
+            if(this.descending) {
+                let sortedSubs = this.lessons
+                sortedSubs = sortedSubs.sort((a, b) => {
+                    return b.spaces - a.spaces;
+                })
+            }
+        },
+        sortSubjects() {
+            if (this.ascending) {
+                let sortedSubs = this.lessons
+                sortedSubs = sortedSubs.sort((a, b) => {
+                    let subsA = a.subject.toLowerCase()
+                    let subsB = b.subject.toLowerCase()
+                    if (subsA < subsB) {
+                        return -1
+                    }
+                    if (subsA > subsB) {
+                        return 1
+                    }
+                    return 0
+                })
+            }
+            if (this.descending) {
+                let sortedSubs = this.lessons
+                sortedSubs = sortedSubs.sort((a, b) => {
+                    let subsA = a.subject.toLowerCase()
+                    let subsB = b.subject.toLowerCase()
+                    if (subsA < subsB) {
+                        return 1
+                    }
+                    if (subsA > subsB) {
+                        return -1
+                    }
+                    return 0
+                })
+            }
+        },
     },
 })
